24f6f81092a852605eefbcbc432c94b5cf5a05b9
OjusWiZard <ojuswimail@gmail.com>
2022-11-17 02:39:00 +0530

    feat (home): added UI
    Signed-off-by: OjusWiZard <ojuswimail@gmail.com>

--- src/App.js
+++ src/App.js
@@ -15,12 +15,38 @@ import config from './config.json';
 
 function App() {
 
+  const [provider, setProvider] = useState(null);
   const [account, setAccount] = useState(null);
+  const [escrow, setEscrow] = useState(null);
+  const [homes, setHomes] = useState([]);
 
   const loadBlockchainData = async () => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    setProvider(provider);
+
+    const network = await provider.getNetwork();
+
+    const escrowAddress = config[network.chainId].escrow.address;
+    const realEstateAddress = config[network.chainId].realEstate.address;
+
+    const realEstate = new ethers.Contract(realEstateAddress, RealEstate, provider);
+    const escrow = new ethers.Contract(escrowAddress, Escrow, provider);
+    setEscrow(escrow);
+
+    const totalSupply = await realEstate.totalSupply();
+    const homes = [];
+    for (let i = 1; i <= totalSupply; i++) {
+      const uri = await realEstate.tokenURI(i);
+      const response = await fetch(uri);
+      const metadata = await response.json();
+      homes.push(metadata);
+    }
+    setHomes(homes);
+
     window.ethereum.on('accountsChanged', async () => {
       const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
-      setAccount(accounts[0]);
+      const account = ethers.utils.getAddress(accounts[0]);
+      setAccount(account);
     });
   }
 
@@ -31,12 +57,23 @@ function App() {
   return (
     <div>
       <Navigation account={account} setAccount={setAccount} />
+      <Search />
       <div className='cards__section'>
-
         <h3>Welcome to Smart Estate</h3>
-
+        <hr />
+        <div className='cards'>
+          {
+            homes.map((home, index) => (
+              <Home
+                key={index}
+                home={home}
+                provider={provider}
+                escrow={escrow}
+              />
+            ))
+          }
+        </div>
       </div>
-
     </div>
   );
 }
